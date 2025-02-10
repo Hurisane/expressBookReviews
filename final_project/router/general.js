68966efd2289d31f9d7b4933a9b5551adfcee98d
Lukarz1 <lukasz_prze@wp.pl>
2025-02-10 21:05:43 +0100

    Improving the scope of Tasks 1-4 using Promises or Async-Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,10 +28,23 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async (req, res) =>{
 
-  return res.status(200).json(books);
-  
+  try {
+
+    const booksData = await new Promise((resolve) => {
+
+      resolve(books);
+    });
+
+    return res.status(200).json(booksData);
+  }
+
+  catch (error) {
+
+    return res.status(500).json({message:"Error retrieving data"})
+  }
+
 });
 
 // Get book details based on ISBN
@@ -49,37 +62,54 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
 
-  let author = req.params.author;
+public_users.get('/author/:author', async (req, res) => {
+  try {
+    let author = req.params.author;
 
-  let booksByAuthor = Object.values(books).filter((book) => book.author.trim().toLowerCase() === author.trim().toLowerCase());
+    const booksByAuthor = await new Promise((resolve, reject) => {
+      const filteredBooks = Object.values(books).filter(
+        (book) => book.author.trim().toLowerCase() === author.trim().toLowerCase()
+      );
 
-  if(booksByAuthor.length > 0) {
-    
+      if (filteredBooks.length > 0) {
+        resolve(filteredBooks);
+      } else {
+        reject(new Error("Books written by provided author not found!"));
+      }
+    });
+
     return res.status(200).json(booksByAuthor);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
   }
-
-  return res.status(404).json({message:"Books written by provided author not found !"});
-  
 });
+
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async (req, res) => {
+  try {
+    let title = req.params.title;
 
-  let title = req.params.title;
+    const booksByTitle = await new Promise((resolve, reject) => {
+      const filteredBooks = Object.values(books).filter(
+        (book) => book.title.trim().toLowerCase() === title.trim().toLowerCase()
+      );
 
-  let bookByTitle = Object.values(books).find((book) => book.title.trim().toLowerCase() === title.trim().toLocaleLowerCase());
+      if (filteredBooks.length > 0) {
+        resolve(filteredBooks);
+      } else {
+        reject(new Error("Books with the provided title not found!"));
+      }
+    });
 
-  if(bookByTitle) {
-
-    return res.status(200).json(bookByTitle);
+    return res.status(200).json(booksByTitle);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
   }
-
-  return res.status(404).json({message:"Book by provided title not found !"});
-  
 });
 
+  
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
 
